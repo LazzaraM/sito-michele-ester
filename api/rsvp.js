27d17c146fd5e13ecf12ece40ab10ce7d83817ac
LazzaraM <64254907+LazzaraM@users.noreply.github.com>
2026-03-08 15:48:03 +0100

    Create rsvp.js

---
+++ api/rsvp.js
@@ -0,0 +1,16 @@
+export default function handler(req, res) {
+
+  if (req.method === "POST") {
+
+    const data = req.body;
+
+    console.log("RSVP ricevuta:", data);
+
+    res.status(200).json({ success: true });
+
+  } else {
+
+    res.status(405).json({ error: "Method not allowed" });
+
+  }
+}
